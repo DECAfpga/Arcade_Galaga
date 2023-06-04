e97c3f31a0c6936384f43bc970c9f7fcaf2525d7
somhi <somhix@gmail.com>
2023-06-04 23:24:31 +0200

    scandoubler.v reverted change. tank nano 20k changed to system verilog

--- mist/scandoubler.v
+++ mist/scandoubler.v
@@ -121,7 +121,8 @@ reg [COLOR_DEPTH*3-1:0] sd_out;
 // ==================================================================
 
 // 2 lines of 2**HCNT_WIDTH pixels 3*COLOR_DEPTH bit RGB
-(* ramstyle = "no_rw_check" *) reg [COLOR_DEPTH*3-1:0] sd_buffer[2*2**HCNT_WIDTH:2*2**HCNT_WIDTH];
+(* ramstyle = "no_rw_check" *) reg [COLOR_DEPTH*3-1:0] sd_buffer[2*2**HCNT_WIDTH];
+
 
 // use alternating sd_buffers when storing/reading data   
 reg        line_toggle;
